eb890a45feae1cdf3d5de8399c136ce5590b1dab
Valeriy <4panin@gmail.com>
2020-04-28 15:22:07 +0300

    second

--- script.js
+++ script.js
@@ -19,14 +19,47 @@ let fruits = ['orange.jpg', 'apple.png', 'plum.bmp'];
 
 //let dialog = prompt("Вам есть 18?", "да");
 
+let dialog = prompt("Вам есть 18?", "да");
+console.log(dialog);
+
 //console.log(dialog);
 
 let incr = 10,
 decr = 10;
+console.log(decr--);
 
-console.log(incr++);
+console.log(5%2);
 
+console.log("22" == 22);
+console.log("23" == 22);
+console.log("22" === 22);
 
+let isMMM = true,
+isBBB = true;
+console.log(isMMM && isBBB);
+
+let isMMM = true,
+isBBB = false;
+console.log(isMMM && isBBB);
+
+let isMMM = true,
+isBBB = true;
+console.log(isMMM || isBBB);
+
+let isMMM = true,
+isBBB = false;
+console.log(isMMM || isBBB);
+
+let isMMM = true,
+isBBB = false;
+console.log(!isMMM || isBBB);
+
+"use strict"; // JS running according to new standart ES
+a = 3;
+console.log(a); // Будет сообщение undefind
+
+let a = 3;
+console.log(a); // Выдаст 3
 
 
 
