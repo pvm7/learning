980517a9665ea99befa369ef94427f6d775f4681
Valeriy <4panin@gmail.com>
2020-04-28 14:52:17 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+/* let boy = 
+{
+    name: 'Petr',
+    age: 34,
+    marrid: false
+
+}; 
+
+console.log(boy.name); */
+
+let fruits = ['orange.jpg', 'apple.png', 'plum.bmp'];
+
+// console.log(fruits[1]);
+
+// let answer = confirm("Вам есть 18?");
+
+//console.log(answer);
+
+//let dialog = prompt("Вам есть 18?", "да");
+
+//console.log(dialog);
+
+let incr = 10,
+decr = 10;
+
+console.log(incr++);
+
+
+
+
+
+
